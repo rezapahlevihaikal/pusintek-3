c1e61220b66d5e7f9529d5b2be4d55e01a6bbfc6
rezapahlevihaikal <eskrimayambawang@gmail.com>
2019-09-30 09:55:15 +0700

    start

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.headers.cards')
+    
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-8 mb-5 mb-xl-0">
+                {{--  <div class="card bg-gradient-default shadow">
+                    
+                </div>  --}}
+            </div>
+            <div class="col-xl-4">
+                {{--  <div class="card shadow">
+                   
+                    
+                </div>  --}}
+            </div>
+        </div>
+        <div class="row mt-5">
+            
+        </div>
+
+       
+    </div>
+@endsection
+
+@push('js')
+    //<script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    //<script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
